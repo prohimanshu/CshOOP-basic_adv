e8f96c9213a7e307554966668310f0fd32eca56c
Himanshu <coderhimanshu@outlook.com>
2018-12-01 14:40:21 +0530

    Classt  complete final code

---
+++ Classtest/Classtest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Classtest
+{
+    class Classt// main class 
+    {
+        public string Name;//each field access as public by default working as private 
+        public string Branch;//stack type memory
+        public double Roll_No;
+        public void Run()//public method not a return type
+        {
+            Console.WriteLine("Name:{0} Branch:{1} Roll_NO:{2}", Name, Branch, Roll_No);// to print pass data
+        }
+    }
+    class Program// main class 
+    {
+        static void Main(string [] a) 
+        {
+            Classt ct = new Classt();//object of class Classt and take a refrence type memory
+            ct.Name = "Himanshu";//pass string data
+            ct.Branch = "CSE";
+            ct.Roll_No = 141340101012;
+            ct.Run();//call the Classt method  
+        }
+
+    }
+}
