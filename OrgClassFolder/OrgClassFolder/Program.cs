d5c59243cc2d58a3aec58eb69980489e55ef3c16
Himanshu <coderhimanshu@outlook.com>
2018-12-01 16:10:15 +0530

    OrgClassFolder 16:09 01Nov2018

---
+++ OrgClassFolder/OrgClassFolder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using OrgClassFolder.com.StudentDBMS;
+
+namespace OrgClassFolder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student sc = new Student();
+            sc.Name = "Himanshu";
+            sc.Branch = "CSE";
+            sc.Roll_No = 141340101012;
+            sc.age = 21;
+
+            sc.Run();
+        }
+    }
+}
---
+++ OrgClassFolder/OrgClassFolder/com/StudentDBMS/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OrgClassFolder.com.StudentDBMS
+{//-------project name---folder-- modulename
+    class Student
+    {
+        public string Name;
+        public string Branch;
+        public double Roll_No;
+        public int age;
+        public void Run()
+        {
+            Console.WriteLine("i'm in class student");
+            Console.WriteLine("Name: {0}\nBranch: {1}\nRoll_No: {2}\nAge: {3}", Name, Branch, Roll_No, age);
+        }
+    }
+}
