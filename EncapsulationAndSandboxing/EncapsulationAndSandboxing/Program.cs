029f02f6ecab5399c290fa5aa9333765deb16b0c
Himanshu <coderhimanshu@outlook.com>
2018-12-01 16:50:50 +0530

    EncapsulationAndSandboxing  16:50 01Dec2018

---
+++ EncapsulationAndSandboxing/EncapsulationAndSandboxing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using EncapsulationAndSandboxing.com.StudentDBMS;
+
+namespace EncapsulationAndSandboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student sc = new Student();
+            sc.SetAge(21);
+            sc.SetMarks(100);
+            sc.SetName("Himanshu");
+            sc.Run();
+        }
+    }
+}
---
+++ EncapsulationAndSandboxing/EncapsulationAndSandboxing/com/StudentDBMS/Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace EncapsulationAndSandboxing.com.StudentDBMS
+{
+    class Student
+    {
+        private string Name;
+        private int Age;
+        private int Marks;
+
+        public void SetName(string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                Name = name;
+            }
+            else
+            {
+                Console.WriteLine("Please do't empty string");
+            }
+        }
+        public void SetAge(int age)
+        {
+            if (age != 0)
+            {
+                Age = age;
+            }
+            else
+            {
+                Console.WriteLine("invalid age");
+            }
+        }
+        public void SetMarks(int marks)
+        {
+            if (marks<=100)
+            {
+                Marks = marks;
+            }
+            else
+            {
+                Console.WriteLine("invalid marks");
+            }
+        }
+        public void Run()
+        {
+            Console.WriteLine("valid data is Name:{0} Age:{1} Marks:{2}",Name,Age,Marks);
+        }
+    }
+}
