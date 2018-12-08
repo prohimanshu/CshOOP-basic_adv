3bd8446dd9e7c4f69e3639c0365ecc7ea2ae8dc0
Himanshu <coderhimanshu@outlook.com>
2018-12-08 13:41:02 +0530

    ThisKeyword 08Dec2018  13:40

---
+++ ThisKeyword/ThisKeyword/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ThisKeyword.com.Module;
+
+namespace ThisKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student sc = new Student();
+            sc.setvalue(21, "Himanshu");
+        }
+    }
+}
---
+++ ThisKeyword/ThisKeyword/com/Module/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ThisKeyword.com.Module
+{
+    class Student
+    {
+        private int age;
+        private string name;
+        public void setvalue(int age,string name)
+        {
+            this.age = age;
+            this.name = name;
+            Console.WriteLine("parss age is {0}", this.age);//access private value age
+            Console.WriteLine("parss name is {0}", this.name);// access private value name
+        }
+        
+    }
+}
