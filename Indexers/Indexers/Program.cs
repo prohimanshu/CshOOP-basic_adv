5347d6110f2f680ca99f0bc168a607bb8704443e
Himanshu <coderhimanshu@outlook.com>
2018-12-10 13:58:34 +0530

    Indexers 10Dec2018 13:58

---
+++ Indexers/Indexers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Indexers.com.Module;
+
+namespace Indexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student std = new Student();
+            std[0] = 2100;
+            Console.WriteLine("indexer value and value is {0}",std[0]);
+            std[0, 1]=10;
+            Console.WriteLine("indexer overload object and stored value is {0}",std[0]);
+        }
+    }
+}
---
+++ Indexers/Indexers/com/Module/Student.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Indexers.com.Module
+{
+    class Student
+    {
+        private int[] array = { 1, 2, 3, 4, 5 };
+        public int this [int index]
+        {
+            get//get the value from class program
+            {
+                if(index>=0 && index < array.Length)
+                {
+                    return array[index];//return stored value in index
+
+                }
+                else
+                {
+                    return 0;
+                }
+
+            }
+            set
+            {
+                if (index >= 0 && index < array.Length)
+                {
+                    if (value > 0)
+                    {
+                        array[index] = value;//include the value in given index
+                    }
+                    else
+                    {
+                        Console.WriteLine("invalid value");
+                    }
+                }
+            }
+        }
+
+        public int this [int index, int i]//indexer overload
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                array[index] = value + i;
+            }
+        }
+    }
+}
