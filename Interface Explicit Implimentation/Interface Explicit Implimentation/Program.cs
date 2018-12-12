1b0d8190eebd1c372d78e009f5a8423f90684439
Himanshu <coderhimanshu@outlook.com>
2018-12-12 13:53:39 +0530

    Interface Explicit Implimentation 12Dec2018 13:53

---
+++ Interface Explicit Implimentation/Interface Explicit Implimentation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Interface_Explicit_Implimentation.com.Module;
+
+namespace Interface_Explicit_Implimentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car cr = new Car();
+            Iclass aclass=cr;
+            aclass.Run();
+
+            Iclassa classa = cr;
+            classa.Run();
+        }
+    }
+}
---
+++ Interface Explicit Implimentation/Interface Explicit Implimentation/com/Module/Iclass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Interface_Explicit_Implimentation.com.Module
+{
+    interface Iclass
+    {
+        void Run();
+    }
+    interface Iclassa
+    {
+        void Run();
+    }
+    class Car : Iclass, Iclassa
+    {
+        void Iclass.Run()
+        {
+            Console.WriteLine("run Iclass");
+        }
+
+        void Iclassa.Run()
+        {
+            Console.WriteLine("run Iclassa");
+        }
+    }
+}
